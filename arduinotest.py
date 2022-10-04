fde5b43a5af2d552f4d9a0a47a6664f4ebee1b00
zeb-kuyper <kuyperzeb@gmail.com>
2022-10-04 11:08:47 +0200

    changed comments and added imports

--- arduinotest.py
+++ arduinotest.py
@@ -1,6 +1,8 @@
-# eofij;sgijgrd;lijs
-# o;esdijflsidfj
+# Group 18-3
+# Collaborative code with Sol Stefen Alex and Zeb
 
+import fhict_cb_01.CustomPymata4 from CustomPymata4
+import time,sys
 import flask from Flask
 
 DHTPIN = 12
