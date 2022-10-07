e799873421288c8b2f0cd38502be97624def4c46
zeb-kuyper <kuyperzeb@gmail.com>
2022-10-07 16:10:39 +0200

    updated code, fake arduino and real arduino scripts

---
+++ Zeb/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, json, request
+from fhict_cb_01.CustomPymata4 import CustomPymata4
+import time,sys
+
+app =  Flask(__name__)
+
+statsDict = {}
+
+
+@app.route("/")
+def index():
+    return render_template('index2.html', stats = statsDict, calculate_average = calculate_average)
+
+@app.route("/data", methods = ['POST'])
+def receive_data():
+    stats = request.json
+    
+    # Extract data without id
+    temp = stats.copy()
+    del temp["id"]
+    
+    id = stats["id"]
+    if id in statsDict:
+        statsDict[id].append(temp)
+        print(statsDict)
+    else:
+        statsDict[id] = [temp]
+        print(statsDict)
+    
+    return "OK",200
+
+def calculate_average(id, measurement):
+     return sum([reading[measurement] for reading in statsDict[id]]) / len(statsDict[id])
+
+    
+if __name__ == '__main__':
+ app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ Zeb/testgenerator.py
@@ -0,0 +1,61 @@
+import csv,time,sys
+import random
+from flask import json
+import requests
+
+generatedList = []
+dataInfo = ["Time","SensorID","Humidity","Temperature","Brightness"]
+headers = {"Content-Type": "application/json; charset=utf-8"}
+
+def getTime():
+    t = time.localtime()
+    currentTime = time.strftime("%d/%m/%Y, %H:%M:%S",t)
+    
+    return currentTime
+
+def getRandomSensorID():
+    newSensorIDs = [112335,2234446,3345557,4456668]
+    sensorID = random.choice(newSensorIDs)
+    
+    return sensorID
+
+def genMeasurements():
+    humidity = random.randint(30,80)
+    temperature = random.randint(18,28)
+    brightness = random.randint(256,754)
+    
+    return humidity, temperature, brightness
+
+
+def loop():
+
+    sensorID, currentTime , humidity, temperature, brightness = getRandomSensorID(), getTime() , genMeasurements()[0], genMeasurements()[1], genMeasurements()[2]
+    data = { 'id': sensorID, 'time': currentTime, 'humidity':humidity, 'temp':temperature, 'brightness':brightness}
+    time.sleep(1.5)
+    response = requests.post("http://127.0.0.1:5000/data", json=data, headers=headers)
+
+
+def data_generator_main():
+    while True:
+        try:
+            loop()
+        except KeyboardInterrupt:
+            print('shutdown')
+            sys.exit(0)
+            
+
+if __name__ == "__main__":
+    data_generator_main()
+
+
+    # with open('./static/generatedData.csv','w') as genData:
+    #     writer = csv.writer(genData)
+    #     writer.writerow(dataInfo)
+    #     writer.writerows(generatedList)
+
+        
+
+# Parse into json data 
+# Call request post 
+# Flask endpoint to handle json data
+
