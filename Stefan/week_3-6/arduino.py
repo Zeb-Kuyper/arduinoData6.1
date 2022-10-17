518ac106a47134503a99444ad7729922cee4d592
S7eFaNS <stefannikola298@gmail.com>
2022-10-17 11:53:44 +0200

    arduino

--- Stefan/week_3-6/arduino.py
+++ Stefan/week_3-6/arduino.py
@@ -1,46 +1,39 @@
 import time
-import sys
 from fhict_cb_01.CustomPymata4 import CustomPymata4
-import requests
+import time
 
-MULTI_SENSOR = 12
-headers = {"Content-Type": "application/json; charset=utf-8"}
+board = CustomPymata4("COM3")
 
-class Reading:
+LEFT_BUTTON = 9
+RIGHT_BUTTON = 8
+RED_LED = 4
+GREEN_LED = 5
 
-    def __init__(self, humidity, temperature, timestamp, light) -> None:
-        self.timestamp = timestamp
-        self.temperature = temperature
-        self.humidity = humidity
-        self.light = light
+BUTTON_PRESSED = 0
 
-    def __str__(self):
-        return f'timestamp: {self.timestamp}; temperature: {self.temperature:.2f}; humidity: {self.humidity:.2f}; light: {self.light:.2f}'
+cooking = False
 
-def setup():
-    global board
-    board = CustomPymata4(com_port = "COM3")
-    board.set_pin_mode_dht(MULTI_SENSOR, sensor_type = 11, differential = .05)
-    board.set_pin_mode_analog_input(2, differential=50)
-
-def loop():
-    global temperature_storage
-    global humidity_storage
-    time.sleep(5)
-    light = board.analog_read(2)
-    resistance_sensor = (1023 - light[0]) * 10 / light[0]
-    requests.post("http://localhost:5000/data", json=Reading(*board.dht_read(MULTI_SENSOR), 325 * pow(resistance_sensor, -1.4) / 1000).__dict__, headers=headers)
-
-def arduino_main():
-    setup()
-    while True:
-        try:
-            loop()
-        except KeyboardInterrupt:
-            print('shutdown')
-            board.shutdown()
-            sys.exit(0)
+def LeftButtonChanged(data):
+    global cooking
+    if data[2] == 1 and cooking == False:
+        cooking = True
 
 
-if __name__ == "__main__":
-    arduino_main()
+def RightButtonChanged(data):
+    global cooking
+    if data[2] == 1 and cooking == True:
+        cooking = False
+
+
+board.set_pin_mode_digital_input_pullup(RIGHT_BUTTON, callback = RightButtonChanged)
+board.set_pin_mode_digital_input_pullup(LEFT_BUTTON, callback = LeftButtonChanged)
+
+
+while True:
+    if cooking:
+        board.digital_write(RED_LED, 1)
+        board.digital_write(GREEN_LED, 0)
+    else:
+        board.digital_write(RED_LED, 0)
+        board.digital_write(GREEN_LED, 1)
+    time.sleep(0.2)
